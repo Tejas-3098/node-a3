03c864ebe8e556fe3dc5fa10b3bc1033bf23b543
Tejas-3098 <47113471+Tejas-3098@users.noreply.github.com>
2022-03-17 14:54:15 -0400

    Add files via upload

---
+++ mongoose/tuits/TuitSchema.ts
@@ -0,0 +1,11 @@
+/**
+ * @file Implements a mongoose schema which defines the shape of the object in tuits collection
+ */
+import mongoose, {Schema} from "mongoose";
+import Tuit from "../../models/tuits/Tuit";
+const TuitSchema = new mongoose.Schema<Tuit>({
+    tuit: {type: String, required: true},
+    postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+    postedOn: {type: Date, default: Date.now}
+}, {collection: "tuits"});
+export default TuitSchema;
